b6c0d18a42720f168e88d1227ca3889abb91a9fb
THANYALAK AMPAI <63010459@KMITL.AC.TH>
2020-08-24 00:11:30 +0700

    scanf again

--- greater/greatter.cpp
+++ greater/greatter.cpp
@@ -1,9 +1,13 @@
-#include <iostream>
+﻿#include <iostream>
 int main() {
 	int num,notme;
 	scanf_s("%d", &num);
-	if (num < 0)
-		printf("please insert the number that is greater or equal zero");
-	else if (num >= 50) printf("Pass");
+	if (num < 0) {
+		printf("please insert the number that is greater or equal zero\n");
+		scanf_s("%d", &notme);
+		if (notme >= 50) printf("Pass");
+		else printf("Fail");}
+	else if (num >=50)printf("Pass");
 	else printf("Fail");
 }
+//P.S.เปลี่ยนเป็นถ้ารับค่ารอบแรกน้อยกว่า0ให้รับค่าอีกรอบ
